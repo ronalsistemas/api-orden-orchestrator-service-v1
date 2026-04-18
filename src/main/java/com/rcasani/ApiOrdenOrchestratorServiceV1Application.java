0ede349f86de1c5e107fb7ee188eb7e2723025e1
Ronal Casani <ronalsistemas@gmail.com>
2026-04-17 22:15:34 -0500

    Proyecto base ms api-orden-orchestrator-service-v1

---
+++ src/main/java/com/rcasani/ApiOrdenOrchestratorServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.rcasani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrdenOrchestratorServiceV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiOrdenOrchestratorServiceV1Application.class, args);
+    }
+
+}
